82c3e6cbcdbf0a371c92fced466287c1208467ea
Nate Trask <trask.nate@gmail.com>
2023-03-19 19:31:44 -0500

    added console.log and conditional statement

--- Develop/script.js
+++ Develop/script.js
@@ -13,16 +13,28 @@ var specialCharacters = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":"
 
 // Write password to the #password input
 function writePassword() {
-
+  
+  //tried using "parseInt" and "parseFloat" did not work
   var chooseLength = prompt("How many characters would you like? Choose between 8 and 128.");
-  if (chooseLength<8 || chooseLength>128) window.alert("Password is either too short or too long.");
-  if(isNaN(chooseLength))window.alert("Invalid Input");
+  if (chooseLength<8 || chooseLength>128) {window.alert("Password is either too short or too long.")};
+  if(isNaN(chooseLength)){window.alert("Invalid Input")};
   
   var lowerCase = prompt('Click OK to to use lowercase letters.');
   var upperCase = prompt('Click OK to use uppercase letters.');
   var numericArray = prompt('Click OK to use numbers.');
   var specialCharacters = prompt('Click OK to use special characters.');
+  console.log(typeof chooseLength)
+  console.log(lowerCase == true);
+  console.log(upperCase == true);
+  console.log(numericArray == true);
+  console.log(specialCharacters == true);
 
+  if (
+    (lowerCase) !== true &&
+    (upperCase) !== true &&
+    (numericArray) !== true &&
+    (specialCharacters) !== true
+  ) {window.alert("Invalid input, must select one or more value to proceed.")}
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
